86398608fbd625e4fc99cf20a2df2d77105a7842
José Carlos <bronx.junior@hotmail.com>
2024-01-19 01:26:35 -0300

    projeto-em-andamento

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'senha e confirmar senha não coíncidem')
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+             messages.add_message(request, constants.ERROR, 'usuario ja existe')
+             return redirect('/usuarios/cadastro')
+
+        try: 
+            User.objects.create_user(
+                username=username,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do servidor')
+            return redirect('/usuarios/cadastro')    
+
+        return HttpResponse("Teste")    
